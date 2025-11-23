52daaea0eded764e2baa827ba685ec1455cdfca7
diveshdp <163938088+diveshdp@users.noreply.github.com>
2025-11-23 12:45:26 -0500

    Created three sample routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-    </>
-  )
+function Home() {
+  return <h1>Home Page</h1>;
 }
 
-export default App
+function About() {
+  return <h1>About Page</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Page</h1>;
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+}
+
+export default App;
