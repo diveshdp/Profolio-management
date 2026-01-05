c55152a0abca3bee5a9717bd812865269d8ce0de
diveshdp <163938088+diveshdp@users.noreply.github.com>
2026-01-05 00:36:27 -0500

    fixed build but need to implement css for the ag grid table

--- src/pages/holdingspage/HoldingsPage.config.ts
+++ src/pages/holdingspage/HoldingsPage.config.ts
@@ -4,54 +4,8 @@ import { sectorColors } from "../../constants/portfolio.constants";
 export const holdingsColumnDefs = [
   { headerName: "SYMBOL", field: "symbol", width: 120, cellClass: "fw-bold" },
   { headerName: "COMPANY", field: "name", flex: 1 },
-
-  // {
-  //   headerName: "SECTOR",
-  //   field: "sector",
-  //   width: 150,
-  //   cellRenderer: (params: any) => {
-  //     const color = sectorColors[params.value] || "#6c757d";
-  //     return `
-  //       <span style="
-  //         background:${color};
-  //         padding:6px 12px;
-  //         color:white;
-  //         border-radius:16px;
-  //         font-size:12px;
-  //         font-weight:600;
-  //       ">
-  //         ${params.value}
-  //       </span>
-  //     `;
-  //   },
-  // },
-
-  {
-    headerName: "SECTOR",
-    field: "sector",
-    width: 150,
-    cellRenderer: (params: any) => {
-      const color = sectorColors[params.value] || "#0d6efd";
-
-      return `
-      <span style="
-        display:inline-block;
-        background-color:${color};
-        padding:6px 14px;
-        color:white;
-        border-radius:20px;
-        font-size:12px;
-        font-weight:600;
-        text-transform:uppercase;
-      ">
-        ${params.value}
-      </span>
-    `;
-    },
-  },
-
+  { headerName: "SECTOR", field: "sector", width: 150 },
   { headerName: "SHARES", field: "shares", width: 120, type: "rightAligned" },
-
   {
     headerName: "PRICE",
     field: "currentPrice",
@@ -59,7 +13,6 @@ export const holdingsColumnDefs = [
     type: "rightAligned",
     valueFormatter: (p: any) => `$${p.value.toFixed(2)}`,
   },
-
   {
     headerName: "VALUE",
     field: "totalValue",
@@ -68,7 +21,6 @@ export const holdingsColumnDefs = [
     valueFormatter: (p: any) =>
       `$${p.value.toLocaleString("en-US", { maximumFractionDigits: 0 })}`,
   },
-
   {
     headerName: "GAIN / LOSS",
     field: "gainLoss",
@@ -81,21 +33,10 @@ export const holdingsColumnDefs = [
     valueFormatter: (p: any) =>
       `${p.value >= 0 ? "+" : ""}$${p.value.toLocaleString("en-US")}`,
   },
-
   {
     headerName: "RETURN",
     field: "gainLossPercent",
     width: 120,
     type: "rightAligned",
-    cellRenderer: (p: any) => {
-      const positive = parseFloat(p.value) >= 0;
-      return `
-        <span class="badge rounded-pill px-3 ${
-          positive ? "bg-success" : "bg-danger"
-        }">
-          ${positive ? "+" : ""}${p.value}%
-        </span>
-      `;
-    },
   },
 ];
